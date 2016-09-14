3288e106b782f501ead73538479dae8d75931c0f
Martin Polanka <PolankaMartin@gmail.com>
2016-09-14 16:24:04 +0200

    Setup tests and travis-ci.

---
+++ test/cz/polankam/jjcron/remote/TaskDetailTest.java
@@ -0,0 +1,40 @@
+package cz.polankam.jjcron.remote;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Neloop
+ */
+public class TaskDetailTest {
+
+    public TaskDetailTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
